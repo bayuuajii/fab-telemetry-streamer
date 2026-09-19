8b047ddf4c86cbe5c068d90ca4594875b3427212
bayuuajii <bayubnery@gmail.com>
2026-09-19 20:11:21 +0700

    feat: implement anomaly detection logic on server

--- server.py
+++ server.py
@@ -1,39 +1,39 @@
 import json
 import paho.mqtt.client as mqtt
 
-# Harus sama persis dengan yang ada di mesin
 BROKER = "broker.hivemq.com"
 PORT = 1883
 TOPIC = "fab/sensor/litho_01"
 
-def on_connect(client, userdata, flags, reason_code, properties=None):
-    if reason_code == 0:
-        print(f"Status: Server TERHUBUNG ke Pabrik.")
-        print(f"Mendengarkan saluran metrik: {TOPIC}...\n")
-        # Server wajib 'subscribe' ke topik yang sama dengan mesin
-        client.subscribe(TOPIC)
-    else:
-        print(f"Gagal terhubung, kode error: {reason_code}")
+# Ambang batas aman
+TEMP_LIMIT = 22.5
+PRESS_LIMIT = 1.35
 
-# Fungsi ini otomatis terpanggil setiap kali ada data masuk dari mesin
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Status: Server Monitoring & Quality Control Aktif.")
+    client.subscribe(TOPIC)
+    print(f"Mendengarkan metrik di {TOPIC}...\n")
+
 def on_message(client, userdata, msg):
-    payload = msg.payload.decode('utf-8')
-    data = json.loads(payload)
-    
-    # Format tampilan layaknya log di control room
-    print(f"[DATA MASUK] Mesin: {data['equipment_id']} | Status: {data['status']} | Suhu: {data['temperature_c']}°C | Tekanan: {data['pressure_bar']} bar")
+    try:
+        payload = json.loads(msg.payload.decode())
+        eq_id = payload.get("equipment_id")
+        temp = payload.get("temperature_c")
+        press = payload.get("pressure_bar")
+        status = payload.get("status")
 
-# Inisialisasi Server sebagai MQTT Client (Subscriber)
-server_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Fab_Server_Central")
-server_client.on_connect = on_connect
-server_client.on_message = on_message
+        # Logika Deteksi Anomali
+        if temp > TEMP_LIMIT or press > PRESS_LIMIT:
+            print(f"[CRITICAL WARNING] Mesin: {eq_id} | Suhu: {temp}°C | Tekanan: {press} bar | ABNORMAL!")
+        else:
+            print(f"[NORMAL] Mesin: {eq_id} | Status: {status} | Suhu: {temp}°C | Tekanan: {press} bar")
 
-print("Menyalakan Server Pusat Pabrik...")
-server_client.connect(BROKER, PORT, 60)
+    except Exception as e:
+        print(f"Error membaca data: {e}")
 
-try:
-    # Loop forever membuat server standby terus menerus tanpa mati
-    server_client.loop_forever() 
-except KeyboardInterrupt:
-    print("\nServer dimatikan oleh Admin.")
-    server_client.disconnect()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(BROKER, PORT, 60)
+client.loop_forever()
