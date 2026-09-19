d870d974dae8a9dd84cd81d24dae0ea108a01654
bayuuajii <bayubnery@gmail.com>
2026-09-19 19:01:16 +0700

    feat: implement MQTT subscriber on server

--- server.py
+++ server.py
@@ -1,29 +1,39 @@
-import socket
 import json
+import paho.mqtt.client as mqtt
 
-HOST = '127.0.0.1'
-PORT = 5000
+# Harus sama persis dengan yang ada di mesin
+BROKER = "broker.hivemq.com"
+PORT = 1883
+TOPIC = "fab/sensor/litho_01"
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((HOST, PORT))
-
-print(f"Server Pabrik Aktif! Memantau aliran data di {HOST}:{PORT}...")
-
-while True:
-    data, addr = sock.recvfrom(1024)
-    payload = json.loads(data.decode('utf-8'))
-    
-    # 1. Simpan data otomatis ke file log pabrik
-    with open("fab_telemetry.log", "a") as f:
-        f.write(json.dumps(payload) + "\n")
-        
-    # 2. Logika Deteksi Anomali (Rule-based Monitoring)
-    temp = payload.get("temperature_c", 0)
-    status = payload.get("status", "")
-    
-    if temp > 22.5:
-        print(f"[ALERT - OVERHEAT] Mesin {payload['equipment_id']} Suhu Tinggi: {temp}°C!")
-    elif status == "MAINTENANCE":
-        print(f"[WARNING] Mesin {payload['equipment_id']} Butuh Perbaikan Segera!")
+def on_connect(client, userdata, flags, reason_code, properties=None):
+    if reason_code == 0:
+        print(f"Status: Server TERHUBUNG ke Pabrik.")
+        print(f"Mendengarkan saluran metrik: {TOPIC}...\n")
+        # Server wajib 'subscribe' ke topik yang sama dengan mesin
+        client.subscribe(TOPIC)
     else:
-        print(f"[OK - NORMAL] Log tersimpan dari {payload['equipment_id']}")
+        print(f"Gagal terhubung, kode error: {reason_code}")
+
+# Fungsi ini otomatis terpanggil setiap kali ada data masuk dari mesin
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode('utf-8')
+    data = json.loads(payload)
+    
+    # Format tampilan layaknya log di control room
+    print(f"[DATA MASUK] Mesin: {data['equipment_id']} | Status: {data['status']} | Suhu: {data['temperature_c']}°C | Tekanan: {data['pressure_bar']} bar")
+
+# Inisialisasi Server sebagai MQTT Client (Subscriber)
+server_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, "Fab_Server_Central")
+server_client.on_connect = on_connect
+server_client.on_message = on_message
+
+print("Menyalakan Server Pusat Pabrik...")
+server_client.connect(BROKER, PORT, 60)
+
+try:
+    # Loop forever membuat server standby terus menerus tanpa mati
+    server_client.loop_forever() 
+except KeyboardInterrupt:
+    print("\nServer dimatikan oleh Admin.")
+    server_client.disconnect()
