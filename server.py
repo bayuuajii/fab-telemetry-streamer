cb2023be69ae82626bf95347272d87aab21695d3
bayuuajii <bayubnery@gmail.com>
2026-09-20 05:12:30 +0700

    feat: finish phase 4 with structured logging and audit trail

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
 import json
+import logging
 import paho.mqtt.client as mqtt
+
+# Setup Logging ke File dan Terminal
+logging.basicConfig(
+    filename="fab_telemetry.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 BROKER = "broker.hivemq.com"
 PORT = 1883
 TOPIC = "fab/sensor/litho_01"
 
-# Ambang batas aman
 TEMP_LIMIT = 22.5
 PRESS_LIMIT = 1.35
 
 def on_connect(client, userdata, flags, rc, properties=None):
-    print("Status: Server Monitoring & Quality Control Aktif.")
+    msg = "Status: Server Monitoring & Quality Control (With Logging) Aktif."
+    print(msg)
+    logging.info(msg)
     client.subscribe(TOPIC)
-    print(f"Mendengarkan metrik di {TOPIC}...\n")
 
 def on_message(client, userdata, msg):
     try:
@@ -22,14 +31,19 @@ def on_message(client, userdata, msg):
         press = payload.get("pressure_bar")
         status = payload.get("status")
 
-        # Logika Deteksi Anomali
         if temp > TEMP_LIMIT or press > PRESS_LIMIT:
-            print(f"[CRITICAL WARNING] Mesin: {eq_id} | Suhu: {temp}°C | Tekanan: {press} bar | ABNORMAL!")
+            log_msg = f"[CRITICAL WARNING] Mesin: {eq_id} | Suhu: {temp}°C | Tekanan: {press} bar | ABNORMAL!"
+            print(log_msg)
+            logging.warning(log_msg)
         else:
-            print(f"[NORMAL] Mesin: {eq_id} | Status: {status} | Suhu: {temp}°C | Tekanan: {press} bar")
+            log_msg = f"[NORMAL] Mesin: {eq_id} | Status: {status} | Suhu: {temp}°C | Tekanan: {press} bar"
+            print(log_msg)
+            logging.info(log_msg)
 
     except Exception as e:
-        print(f"Error membaca data: {e}")
+        err_msg = f"Error membaca data: {e}"
+        print(err_msg)
+        logging.error(err_msg)
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
